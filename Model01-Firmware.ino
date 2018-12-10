c068254b4d266525f0a588c052abcb85604cf666
Ian J. Watson <ian.james.watson@cern.ch>
2018-12-11 00:43:51 +0900

    change the thumb keys and add some emacsy macros

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -87,9 +87,10 @@
   */
 
 enum { MACRO_VERSION_INFO,
-       MACRO_ANY,
+       MACRO_CU,
        MACRO_MX,
-       MACRO_CX
+       MACRO_CX,
+       MACRO_CC
      };
 
 
@@ -176,15 +177,15 @@ KEYMAPS(
   (___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
    Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
    Key_Tab,   Key_A, Key_S, Key_D, Key_F, Key_G,
-   M(MACRO_CX), Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
-   OSM(LeftControl), Key_Backspace, OSM(LeftGui), OSM(LeftShift),
-   ShiftToLayer(FUNCTION1),
+   M(MACRO_CC), Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
+   OSM(LeftControl), Key_Backspace, OSM(LeftShift), OSM(LeftGui),
+   M(MACRO_CX),
 
-   M(MACRO_ANY),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         LockLayer(NUMPAD),
+   M(MACRO_CU),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         LockLayer(NUMPAD),
    Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
                   Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
    M(MACRO_MX),  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
-   OSM(RightShift), OSM(LeftAlt), Key_Spacebar, OSM(RightControl),
+   OSM(RightShift), Key_Enter, Key_Spacebar, OSM(LeftAlt),
    ShiftToLayer(FUNCTION2)),
 
   [NUMPAD] =  KEYMAP_STACKED
@@ -298,9 +299,16 @@ const macro_t *macroAction(uint8_t macroIndex, uint8_t keyState) {
     Macros.play(MACRODOWN(D(LeftControl), T(X), U(LeftControl)));
     break;
 
-  case MACRO_ANY:
-    anyKeyMacro(keyState);
+  case MACRO_CU:
+    // macrodown only sends one command even if held down.
+    Macros.play(MACRODOWN(D(LeftControl), T(U), U(LeftControl)));
     break;
+
+  case MACRO_CC:
+    // macrodown only sends one command even if held down.
+    Macros.play(MACRODOWN(D(LeftControl), T(C), U(LeftControl)));
+    break;
+
   }
   return MACRO_NONE;
 }
